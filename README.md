943648bd8c03a44c352fdf6cace0b3361e861d9f
Samuel de Souza Costa <samuel.costa@accurate.com.br>
2022-07-23 17:55:55 -0300

    Aula 1 com desafios

---
+++ README.md
@@ -0,0 +1,15 @@
+# Projeto Alura - Imersão Java
+
+Evento realizado pela Alura entre os dias 18/07/2022 e 25/07/2022.
+
+## Aula 1
+
+Desafios 
+- [x] Consumir API com lista de filmes
+- [x] Decorar resposta no terminal
+  - [x] Decorar com cores e estilização na fonte do terminal
+  - [ ] Decorar com emojis
+    - Não consegui exibir emojis no terminal do VS Code mesmo alterando as configurações de exibições
+- [ ] Colocar chave da API em arquivo separado
+  - Não realizei esse desafio porque a API do Imdb estava bloqueando novas solicitações 
+- [ ] Mudar o JsonParser para outra biblioteca
---
+++ src/App.java
@@ -0,0 +1,47 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    private static final String URL_FILMES_IMDB = "https://alura-filmes.herokuapp.com/conteudos";
+    
+    private static final String ANSI_REDEFINIDO = "\u001b[0m";
+    private static final String ANSI_NEGRITO = "\u001b[1m";
+    private static final String ANSI_TEXTO_AZUL = "\u001b[34m";
+    private static final String ANSI_FUNDO_AMARELO = "\u001b[43m";
+
+    public static void main(String[] args) throws Exception {
+
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+        String body = executarRequisicao(URL_FILMES_IMDB);
+
+        // extrair só os dados que interessam (titulo, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        // exibir e manipular os dados 
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(ANSI_NEGRITO + "Título: " + ANSI_REDEFINIDO + filme.get("title"));
+            System.out.println(ANSI_NEGRITO + "Cartaz: " + ANSI_REDEFINIDO + filme.get("image"));
+
+            float rating = Float.parseFloat(filme.get("imDbRating"));
+            System.out.println(ANSI_NEGRITO + "Classificação: " + ANSI_REDEFINIDO + ANSI_FUNDO_AMARELO + ANSI_TEXTO_AZUL + rating + ANSI_REDEFINIDO);
+
+            System.out.println();
+        }
+    }
+
+    private static String executarRequisicao(String URL) throws Exception {
+        URI endereco = URI.create(URL);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+
+        return response.body();
+    }
+
+}
